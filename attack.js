4e5b224380e598342b6fe8fd6abf5ff65532047d
Alex Wang <bejajahehe@inaby.com>
2017-04-06 20:12:30 +0000

    add values and damage count

---
+++ attack.js
@@ -0,0 +1,16 @@
+var P1power = null
+var P2power = null
+var P1win=false
+var P2win=false
+var gamecontinue=false
+var kaka='mdzz'
+
+
+if (P1power>P2power){
+    P1win=true
+}else if(P2power>P1power){
+    P2win=true
+}else{
+    gamecontinue=true
+    kaka='zhu'
+}
